e32cf7bd302bbaf9e9b1f6109d97571ad3da1e1c
Maibel <camilamaibelromero@gmail.com>
2025-10-28 13:05:29 -0300

    Actualizar send.js

--- api/send.js
+++ api/send.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     const data = await resend.emails.send({
       from: 'Contacto Web <contacto@mairomero.com.ar>', 
-      to: 'camilamaibelromero@ejemplo.com', 
+      to: 'camilamaibelromero@gmail.com', 
       subject: `Nuevo mensaje de ${nombre}`,
       html: `
         <h2>Nuevo contacto desde la web</h2>
